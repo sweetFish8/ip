ac4ec149033394ad68889a44e77d8d3cbbba6252
Ayu <achanhaaaa@icloud.com>
2025-02-02 04:34:21 +0800

    updated logo to be bigger

--- src/main/java/Sparkle.java
+++ src/main/java/Sparkle.java
@@ -1,46 +1,81 @@
 public class Sparkle {
     public static void main(String[] args) {
-        String logo = "QQQQQQQNggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggNQQQQQQQ\n"+
-"QQQQQNNHiiLL777cc7cuIsFfIIYjeziijzjjFsurrrrrrrrrrrrrrTLTirrrr111|||vi7IzIzFoS4jJxllIIfYFFFfvpNNQQQQQ\n"+
-"QQQQNM8lLTttttttttcy5AaaA22o2a5SEPkPj-mT^^^^^^^^^^^^v^--^vr^^^^rrvTFyoZGekkkBBbk4ZwXPkkEE962t8MNNQQQ\n"+
-"QQNMbAcLcLiivv||rrr^35eoooooooooAPXU-4aUi^^^^^^^^^^^j----||rr^rr1vLJfCeOjhkZERQ64AAAAAAaaa4S2JoHMNQQ\n"+
-"Qgzv||1rrrr^^^^^^^r2tlpw22222ooee25XLhe2k1^^^rifaS22c ---tM4rrr^^^rrrrrqeaXUmk8RZa4aAAAAA222AAoYxYAQ\n"+
-"NRLr^^^^^^^^^^^^^Yyymp6EqHq9kkkJrtezca2e6JJXdmPZp8bOr - -tRRRr^vTr^^^^^e635hkE2BPstsuyoo22222222oxIQ\n"+
-"MRL^^^^^^^^^^^^^yyOH6G6UXZS4PXCCGkZA2ZAURRRR88888O8br   -LR8R8|^|mUDAJ|UmjeSeuwRdx^^rrr|vitIzCyyeJfN\n"+
-"MRL^^^^^^^^^^^rUG8H64ooa4SPkG6kmUX5UpO8888888888bHb8r   -rO88RI^^^oR8zt2kajeaXOV5X^--^^^^^rFr1|e9vfN\n"+
-"MRL^^^^^^^^^^^8888aoA2o2AAA2APGP6Uwm888HHHb88HdDHHHOz   -rm8RX^^-^^vr-^--^vnCAwk2Or^^^^--^oACrSG3rfN\n"+
-"MRL^^^^^^^^^^rQHU2A22222AA5Paeo2ToR88RbbbO88RRR8888Om- --^S8R2^^^^^^^^     -|y25Upq^^^^rTaoSGG99I^YN\n"+
-"MRL^^^^^^^^-^em45w5Z55wPP2C3cFrzRR8808HdH888OHdpqqpHR^ --^x88DI^---v-       -iF24kmU^voXwU69E6h4|^YN\n"+
-"MRL^^^^^^^v2EPPkPPPPX55AyF|^FrORg8wASG88888HpppppppdO5----rpHddavLv-       - ^oFj5UG4ASSrCGUS239i|FN\n"+
-"MRc^^^-rxj33F5OpppHPZAoeTv|SLg8CvFmR88888pmmGmHbpqppd8v---^zbOu^r^^         --r^-vyE2X6A^GAyyykHbjsN\n"+
-"MR7^^^^------vQO9dPooyoelTApVr|ZROHOpO88Hm6q88HmOHppdHm^---r^-^|^           -ryr-^^CUAx^Pka229UEp4sN\n"+
-"NRT^^--------^Qpkq2oeeAyCev^JO8Hb8kebR8Opm88ODb8qpdqqGby-^^^vrr^-----       -^---^^TmocjZy3yVV33yssN\n"+
-"NRT^---------^gGkk2oy2kF^^z99dH8DlvPR88pO8OOdOpmppmmmGpHe^T^               -^3r--^-LqzXwr74oeyyyeFsN\n"+
-"NRT^----------8kkS2Ae|--|IoGb88avvvvF8DOdOOdHdqGdmGG9mqqmI-                 --^^iv^tAUmu4^uahkPXXysN\n"+
-"N8T-----------GwkkF^-rCpOGGH88ObOEtvLHa5HqdqppqqpGG66G6mq|     -              --^-^FyGOUP52UGS455ysN\n"+
-"N8T-----------4P|^JE08ReSHdbb88O88Jl8T|vvqH5OdGqpmGG69m9Gj   -^-         ^-  -------t66GkSSeeX96m2sN\n"+
-"NRT----------rrLmgRRR8t6bHdqOHdbbqE856Hpm82tHm4pdpqmqEGE9P-  -^^--       -^v|r^-  --rqowASe2eeyUq2sN\n"+
-"MRT--------^vz8R8pGkOvmpDmqbqmpHqqHqbODbbqi|IC|iEHqpqE4kEU^--^r^^^^^^^^^^^^r|||J^ --rGjozSP2yee3mAsN\n"+
-"MRT-------tomjSSaAzE|XppmmqGmmpmHm9mmqGOqqwvSLT7|ukAeGzf6E2Cj|F3|tAoSPPZoyjj3343r---r62xePkUPa2yASsN\n"+
-"NR7^-----xUr--a44CkvSbpqqq96GqmmwP99mirDmmEGX69G4i|vAUj|EUk23^1a3lz2r-- --------^---^d9a3kkkkkUXyasN\n"+
-"NRi^----^e----PSAGtk8ppqd9E9GmmPkw67^^UkOk59P9PPkkozmk3zAjaZPl^lv^rCC-^^-------------4p6eekUkkkky2sN\n"+
-"NRT---- v--- rPS6id8ddpH9UEEGm9aP7-  -kEGkkkPPZ5wkEPHPwGAs22aoy|A4cLr----------------rMjooAwkPPPe3sN\n"+
-"MRi ----^----amaFROpHHO9UkkUGqAU|--  ^EsZa5w5S2ASwX5p4w5ww5ElaAAzav-----------^^^^^--tp8ejeASXPkAfsN\n"+
-"MRT---------tk3qROO8R896kEGqqmD8O88Z7j4vS2U4a2eoAaaSU2aA4A4S^LmXkmb8@g88OdO8Om2Ftt|r^^^k2CyyaEEk5JsN\n"+
-"MRT-------^v29RRRRRR8pmEPkk9mw^^COZ5XqkVAeU2oyeyoAeC2e22AAZoFrkmm9qp8RpmmmO8RdA|GObG2tjIeyyyo2aGmzzN\n"+
-"MRT-rTvvveEmOO88888GE9dUPXPpv2- zC^vPCGpAAiyeuCuyoA|43eyeyw2|-^k8mGq8OdqmH888yYJTymRR5TJeyye222kkrzN\n"+
-"NR7---^|JmpD88mpU2uy66bkwwk8--r--u^-^^r2|r^^IV3uCoa-CyeVyyc2F -^6bbqHppmmb88m^ -^rvpR82uoyeeA22mr^zN\n"+
-"NR7----ryooCPPGS4wSAGpbUwhk8^  --^^^^^r-      -^vAt rAeCyeto4- -rHpGDqpGH8OOf^^^^^vwR8Oe2yeoSo2k-^zN\n"+
-"MRtmyxPZPPeYE5mGkXA2mHbGZw6Y^ ----r-               ^1Jkkkuo5mpAj^jpO6Dqp8HGOx|rLvveq888q2ee2hy5L ^zN\n"+
-"MR7ZAoaqwE9PGGGkwaeGOdHpwwmx|                        -4|LajA59UmPCmkmmqOHGmUauAATyAPOO8R9yoAPVk^-^zN\n"+
-"NRTIIHG9XP5UmmGUkPkvpqpdPPUrv-                        r--^-7G^-ok5kkGGpGkkOeoAkSz|SaSGOb8qo4PCS--^zN\n"+
-"NRcIDm9UkZGDGG9EG2L7CHpp9Ua^^r              --        --^r|^^^xvL5w9GqU5wGP2A4kalcoyoaZkpH8qEyo--^zN\n"+
-"NRTmmGEaGUb5e99G2jr^^DdGGUa^^r^           ---        -^  ------cPk9mU445wPS4SSPoaxoyyeeo5aXmpbZ^-^zN\n"+
-"N8TGm9P2ap5eyP9PT|^^^|89EXO^^^r^     --                     -^wPGUPAAAS4soa4whko2Auyeee2we25ZEL -^zN\n"+
-"MRTG9kUL^Uweuapijr^^^^SmUkqy^^^^r^    --              -^r1Lyw5FPSAAAASVc-zy2ZwhPmjAyeeoAPoA55kc -^zN\n"+
-"MRTkk9|-^^qPl2krT|^^^^^6Ek6kt|r          -              ^^^^-^^Z2k3|^^^^^^^^^wEkf7|  zPxx6nigBx2s9XyN\n"+
-"N8Tg2^^^vqv++++++nrr+^^^r        -    ---       --  ------YqDpc^vkk^rqaHkG4e-|r5pGLh3vvqq44--^z3o2-sN\n";
+        String logo = "QQQQQQQQQQQQQQNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNQQQQQQQQQQQQQQ\n"+
+        "QQQQQQQQQQNR5AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA2222222222222222222222222222222248NQQQQQQQQQQ\n"+
+        "QQQQQQQQNMMwLviiL7TTTT7777cLuozC3CCnjjjuCeAAyt|Lyyeeoe33y4vrrrrrrrrrrrrrrrrrrrr|tvv7t|rrrrrrrrrrrr||vvLTtFSCzeoeyUmm9aCFzzzjjuCC3yyyyyeyIvSMMNQQQQQQQQ\n"+
+        "QQQQQQNMMgmJiLTTtttttttttttTJX5Aaa444AA22o2AwU4S6PPkP5Pr^SSr^^^^^^^^^^^^^^^^^^rJ^ --^vir^^^^^^rrr1|LtIuyeAOP2PUkZpBBBO954S5XPPkUE9666GGGUYtGgMMNQQQQQQ\n"+
+        "QQQQNNMMR5TL7TTTttTTT7cLLivv|ia42222oo2oeooooeoSPPkPPm|-tdEer^^^^^^^^^^^^^^^^^ir---- ^r|v^^^^^rr|vLtFCe2AAHGeZPU5ARBBQqU4aS444SSSSSZ5wXPP5FcSRgMMNQQQQ\n"+
+        "QQNMgRH4JiLLLLivvvv||1rrrrrr^^LE5eoooooeoooooooeo4PhPk^^ko49e^^^^^^^^^^^^^^^^rJ^-----^7Trrvr^^rrr|vvi7tIzs9be2XPkZAURBR6h22AAAAAAAAAAAaaa44ettApRgMNNQ\n"+
+        "QQRAFTvvv|||rrrrrr^^^^^^^^^^rwevu95oeoeoeeeeeoooeeoaP4ioZooAkC^^^^^^^r|fASaoCAs------^yMHv^rvr^^^rrrrrr|||u8AVZXPEGGSOQqwaaAAAAAAA2AAA2AAAAAAostTI34RQ\n"+
+        "NQ8J|rrrrr^^^^^^^^^^^^^^^^^eZLTeU88Hmk5SZZ5S4aAAAo2a24LF5oo2Pkr-^rtoSAyA69UU5wT---- -^2R0Q5r^||^^^^^^^^^^^rqEjAwXUGpUSRgh4A4SSSS4aAAAA22222o2222oeLLON\n"+
+        "NNOJ|^^^^^^^^^^^^^^^^^^^^vZIf98O6kPk9b88mGUkkkVr^rFousttA2oePPlomOO88dGkU88HHpi-    -reRRRNEr^^F5t|r^^^^^-^kHey5wSaGau8Bq3tvctJYzCyeo22o22222222o27cbN\n"+
+        "NNOJ1^^^^^^^^^^^^^^^^^^^lyF6RbUUmpqG9kw5SSkkkC|3EkXS2ye4422PORRRRR888888OD88Hqv     -ru88RRg9|^^rmmyXUzTvrrG82joSS3jzoRQR2i^rrrrr||viTJIzCVyeeeeeo7cbM\n"+
+        "NNOJ1^^^^^^^^^^^^^^^^^reyP88mmDEa2AAAaZwPPEUXkqE5Pk99kkUU68RRR8888888OdDO8O88Ov     -^xO888R@kr^^^2gR8R8Fe8OUaujyASSA6qjaPt^-^^^^^^rrrrrr|vLtlIj42LcbM\n"+
+        "NNOJ1^^^^^^^^^^^^^^^^rXGH8Obmw22222Aa44ZXk6G9UUGmEZa46qO8R8888888ObO8888ObHHb8J-     ^imR88R8f^^^--|mRR5vrv3PkaCje2SkHmYApPr------^^^^^^TA1rrrLUmJ|cbM\n"+
+        "NNOJr^^^^^^^^^^^^^^^^XQ8bR8ko2A22oo2AAAAAA2Aw9G9E6mUPGO888bHbbHH888HHHHHHbHHHOe-     ^|PR88R4r^^--^^rVv-^^^--^rYuzC24w9PnkOr^^^^-------TPSe^^jEm5|r7bM\n"+
+        "NNOJr^^^^^^^^^^^^^^-|WWD8H42A2222o2AAAAAAAX66AC5myJw8R8888HHOHbbDdbObbOObHddpbP^     -rARO88x^^----^r^1v^-     -^7jje45k998Gr^^^^^^^-^CZoy5ASGEm2^r7bM\n"+
+        "NNOJr^^^^^^^^^^^^^^-JB0pw2AA2222222AAa4PUP2jf26srsORR8808bOObO88RRgggRR8888888O|  ----rj888Oj^^^^-^^^^^-       - -^tyyA5PUmpC^^^^^^vyZ2o4UmG96UGe^r7bM\n"+
+        "NNOJ1^^^^^^^^^^^^^-^4RkA4ZZSSSSSZ5Zwkk5yIjCYyI^c8RRR88RR888ObbOO888OHddpppppH8Ra- ----^Lm8O8UJ^-----^|^         - -rTfoASXEmdy^^ru5SA5UGG9E9GESXx-^TOM\n"+
+        "NNOJr^^^^^^^^^^-^rJwkShkkPPPPPPPkPPS2C3eF1rTrrGNRR8OR0OGPkGH88888OHDDdppqqqpppbHr ----^rS8bO8moLr^^rTr            -^v22e45kGqksCw4PEPAP6969E52eUT-rTOM\n"+
+        "NNOJr^^^^^^--^1f56kXkkPkPPPPPPwZ4SSA2jvr^|2^TWRRMgmoFs4m8RR88888HqmqpqppppppdHORa- ----^lHOdpqHy||x|-            - -^ceLLyPPm9AAAAa2^^kG9hAee3Am3LctbM\n"+
+        "NNOJr^^^^-^T4k9EUEEkGmmGqpHOq6UwaAoyJvv|vA|eMWWmIrvS8gR88888R8pmmmmmmqdODpqpppdbHL-----^rkOmdUr-rrrr-             --^rr^-^e2PGaoG9py-TmX2yyyC2q8bettbM\n"+
+        "NNOJr^^^^^----^^^^-^4QRbq6pqaoeeyyoVJTccEePMqs^^j8R88OO88888OpmGGGGp88OqmqDDppdHOpr ----^xPCo|-L|-                ^^rLv^--rvfmGz4mkrrP4yyyVekq9EpRetbM\n"+
+        "NNOJr^^^^-----------uBRD6UdU2oeeye23fxLA8Gz^-|908bHHObqq8888HqmGGHRR8Hmp88dpdpppb8u-----rr---^^|r-                -^vu7r^^^^rnm25s^|96kha2Ak6kE6mb3tbM\n"+
+        "NNDJ|^^^------------LQRpEGma2eeee2ACYj4ar-^C88OHbHO8PCER888OdmGHR8O8Hq88pmpDpqqqmdq|--^r-^vcvvr--------            -^-^--^--rIpPoiFUwoyy3yyy3Cy3y2ItbM\n"+
+        "NMdJ1^--------------|MRGkmU22oeeoAA2Sl^-rkR8ObbHO8ktTUR8888Hqd88O88qD8dmqppqmmqmGqO9rr^^v7^- --^^^-----           -^rr----^^repAzky||2233yyyyyyyyyxtbM\n"+
+        "NNbJ|^--------------rRRUPmw2AeyyAU4|--vSEASmpHb8pnvvlm8888Op88OO8bpb8pmmppqmmmmGGqHH9|rYr-                       --io|^--^^--IO2ySa9T^Jk42oeyyyyeoltbM\n"+
+        "NNbx|---------------^b8PPGa22y4ac^--^vLI46HHb885LvivivldRbH8bHO8HdbHdqmmppmGGG6GmDmpqwl^                         -^^^^rrv31^^J52Gq2o9v-zU5kkPwZSZhstbM\n"+
+        "NMpJ1---------------^k8ZkE24Uer- -^vAq8G6pH8R8ZzsJcivvvo6bDGHO8HqppHppmmpqGGGGGGmmmpqa^        -                       ^^Lr1|Sa4p8kC3oIxkSooAZk9E9jtbM\n"+
+        "NMmJr----------------AOahmUJ^--r2m8RHkEqdH8888RRRRdFvvvS8oJkd6mHpqpdppmqdqG6G996GUGGmkr      ---                     - -^---rxCAmHO69kayokHpXS54aSFtbM\n"+
+        "NMqJr----------------jpqar-^tU80R88Aj9OpDOOO88888Rqt|7qHx|vvvsH8EEbHpGmqpqmGGG9G9GmU6E|     ----             --     ---- ----^fSGmD9PkPZeCySmGP9mmutbM\n"+
+        "NMpJr----------------jC^^lm00R88RpIVbbDHHdDOOHHbb88AoR9ttCAyY9RmFa8Od6mpppqmGGmmEmGk9GJ-    -^^--             ^vr^---  -------^AqPEPP5eAa2eeyAmpGmutbM\n"+
+        "NNHJr--------------r|^|U@0RRRRRNGc28HbHpqpObppHbbOEE8H9q888qpOOavIGp6eSqDpqqmmq6EmU9Eme^    ^^^^^---           -rttTvr^-   ---^ap24ae4woyeeeeyykDputbM\n"+
+        "NNOxr------------^|rtpR88888dqRPvA8dHDmmqODmmqdHpGq8pmbOObdH8bGI||vSz||smbpqppqk9P56E6Sr   -^^^^^^^^^^^^----^^^^^^rrrvxI^- ---^A6j2ozyPPAyeeeeyy9OCtbM\n"+
+        "NNOJr-----------|tFpWR8qEwA3sPP|zHpppmGpbqmmqpDqmHbmmqpDpp8RHmGu||yS||v|c4ddpHmP6ee5UEPt^r7xFYlvr|r^^^^^rrr|LYeaPE69aIvvT- ---^SkjyFsAwkU52eyeeuAbytbM\n"+
+        "NNOJr---------^jA9pzrJ54aaozSP1lHpqpmGqqGGGGmpqmHq9U9mmGdmhppmpGntwjitIT||f42llkUnJt9EUPozovrl3CT^lGDqHOOpG5u7v|r|v|vsPa|-----^5Eoyt3wkkkUUk5ao3eqytbM\n"+
+        "NNbJr---------FpEv---jk4Sa3AUvTdbppmmpq6G6Gmqqmq95wUGk6qx^2O9pq6UmP5mGGGa7||vvzdwVT|XGUmkyev-^e2elCje2r^---      ------^^-----^odUkfCPPkkkkkkUU4e6ytbM\n"+
+        "NMptr^-------vqt----^Ak44eaGvJ88ppqmbp69E9GmqmqkSk6UGqPTrtmPmGEUUG596kkk9ksvvLaH4yv|A62CV2yeFLs2Jr^rYoz^ ---^^-----------------i8mO9FekUkkkkkkUPek3tbM\n"+
+        "NMptr^-------h| ----^5XSAaELC88DppqHH6EEE9GmmqkPkZXGC^--^5mm8GUhUUw9kPPPPk9PAyUp5oj4asyX99E4v^^jIrrrtje|^^^^-------------------^GRGwaeyPkUkkkkkk25jtbM\n"+
+        "NMpJr-------rJ------ikSA9UvaR8dpDpDbGEUUU9GGmqka56z^--  ^SSpOUGUGPPkPwZwwPU99Pm9PSUq4lCSXaoooCvjASS3Cui^^-----------------------tgPsooSo4EkPPPPP2astbM\n"+
+        "NNHJr ------rr---- -oEw8e|UROHHdqHOGUUUUU96mmZ5wt^-     rkaaPh4ZUXP5waa4ZPPkXPpwPwkEU5oCeAkPCXf^Jo2r---   ----------------------|H8SyzC5aeXkPPkkA2ItbM\n"+
+        "NNbT^ -------^-----|kqGcjR0bppDH8OG9UkkkUU9Dm4AUCr^-----vUofa5ASka5a422A455w4PmAZXSZ555X9U7e4CPUeePe^^-------------^^^rr^^^^---^5HH8ajuCo5aaPkkk4yJtbM\n"+
+        "NNbJr-------------^kHuFp0Rbpb8R88m69PU6GqmmHqGmpH8Oqkev^IPsTS425UASAA2y2Aaa4ePGoaZAaS4a4Sk1^4G4a5UqO888ROmmqG99mmpmGwyIv||v|r^^--^i6XuCyyya99PPkwCttbM\n"+
+        "NNOxr------------^eosGRRR88RRR88H6GUkkkE9mpm6PAoSbOObEGHpwvLk2ePkAAoooye22A2eZZ3AA2a4aa4ASv-^9pqG6GpDdOg8qqmmqmb888ROXSH86wVLrrr^roZ33yyyee2S6q9kATtOM\n"+
+        "NNbJr----------^ifyG8RRRRR88R888GGqkPPPPkGG6j^--vOma2aaA6mGUay2w4ooy3eyye2AoyFyCoAo2222SA2AztoDGH69mpd8ROppmmmmb8888mUc^e8dbRROPCCt7yeyeyeo22eSmpGltOM\n"+
+        "NNOJr-rr^^^rrijakp88888888888bmqEqmkPPwPU9lse^  vpsrrCS|zqpOXjAYJ2oCCCCCye22C|j3e2yeeee5Aat--^COOpU6mqO8DpdmGmDOR88qa2AuIy5q8R8Hzi7lyeeyeeo22o4k6jvtOM\n"+
+        "NNOxr--rJeX6mqH8ObOOO88888qPoja96bmPwXwPGX^^vi- ^ji^-^eAitZmPFCTrAAnjjuu3eeaI^teVoCyyyyooAz----n8O8pqmH8pppmmpb8888Vr^-^r|Js9WRRpCJuoeeeeo2222ADA-^tOM\n"+
+        "NNOJr-----^^^zmGGmH88GkdPyzjeA66mbmPwXwkmU^ -^^--|F^-----^zL^r^--^vYeeyuny24|-|ay2VyyyeLL2o^ -^^SpqOHqdHqdpGmD8R88Ov-  -^rr|TDW88Ujy2yyeeoAA2e5q| ^tOM\n"+
+        "NNbtr------^l2yoyC4Gk6mAyaw5aa9GpHmPwhhkqp^ -  --^^r^r^-rT^   -     -^rLC4G2^ ^2nCCjyeAvr35|- --^kHpmmbppHmGpO88H85r^^^^r^^|l6g8ROAe2yeee24aeyGa^ rtOM\n"+
+        "NNOlt|^-^|A4eA423jZ6h9kS6kSaA4GmDbpk55Pkm2r  ----- ----^--              --r^--^oX4w4A24GGZ5Sv^-^^vRpdqmGDpmpH88qp8yr^^^v^^r|ud8888pS2eeeo2SaVypJ- rtOM\n"+
+        "NNOxCGqEPaS969Z2njw9Pqp9UkwaeAqpHbH95Zw99Lr  -  - -L^                       -rv|cXUZkhlzPPmpqqOP|^ebpH9mpmppO8mqpbui|r|fxLF4mq88888qAeeeoAh4uAk^  rtOM\n"+
+        "NNOJjPSAoyPmXkGmGk99mm99kS2eem8pHHbmw55G9||-                                    ^E2TxAlF4SS9qmGmwufGd9GqmqmO8GG9HEZ2L2qFvt44PGH8O888EeeooaP4u4A^  rtOM\n"+
+        "NNOJF4SaSGm6XhPX45Gq66U55XSa6PqHpHHqPww9HPo^                                    ^Ir-^Je1|eP6x|oEEGUUEkmGmGp8E9EqqyC2PA5SavT5uoG8OOO8RGee24PSCZJ---rtOM\n"+
+        "NMHJT|vP8Gk9PXP5XmqmmGGUkkEE7|68mpppUwkG|^rr                                     ^^-----^uGi--^Ska2PhEGG6pHPPk68wVo2A5Gus1LeAXA4GDHO8RHAoZPSyw|---rtOM\n"+
+        "NMDJ|LqHmGPEUhZkqpHqG99EEGAvTsCdqdpp6k9kr^^i^                     --             --^^^^vvr--^teIvzkSP66Gpq55wkG6A22A4wquliteyye4wwXEqpO8Hkw42hr --rtOM\n"+
+        "NNHJybqmm65k65Ppbpaa9E9EmoLVv^^PRqqmm9Ukr^^r|-                 ------           -^- --^^^^rt|^-^jwSU99mqk4ZZPU6P2aaaSwmYjJtyyyeeee2hGGqb88RbkXr---rtOM\n"+
+        "NNbJ3qqm6E5aGwmO9ey5U99p2Lyr^^^v8OG6G6kkr^^^rr-                - --             -^-  - ----- -r45P99GmkAa44X55wS4S5SShEYaAlyyyyeeee2Zoe25E6m8qJr^-rtOM\n"+
+        "NMpJCmmG9kXeXGq92yy4kkqkt2|^^^^^j8mE9U5bT^^^^r|-        -                             ----  ^jwhEGGE5AAAAAha2Z24S45XwkZy2ya3yyyeeooAw2eoa55wGy- --rtOM\n"+
+        "NMHJCmG9UPGyrkpaAyu259qLty^^^^^^^U8EEUwbGr^^^^rr^-      ---                            --^rf59GGEka22AAAAwo3r|ooAa5XPEAwEt4eyyeeeeoAXAe245Zwp2- --rtOM\n"+
+        "NNOxCG9kP9o^-1mG2ynyap4rTj^^^^^^^LbGkEP9qu^^^^^^^|^       --                     -^|itzo5UUPeie5a4AAaAAaPufr-^3CeAS5wwam8eu4Cyeeee2aPSe2S5w5Go- --rtOM\n"+
+        "NNOxuUkPmC^-^-|8q2YjaGF^vVr^^^^^^^zdEUX9kkt^^^^^^^r|-          ------               --^|Iv^- ^PZZaAaAa5P3Ir  -IyC2aZw4UG9EySCyeeeoA4Pwo2S5556S^ --rtOM\n"+
+        "NNOJjXk9t^-^^--rGGaz2klrrAv^^^^^^^^2qEP6mP6f^^^^^^|ke^-                             ^r1^^^rr1ySS4AAASPUAzr-   veu2A4SUpkZG45ACyeeoa4XPA2S555k9|   rtOM\n"+
+        "NNbJj9P|^-^^--  rmSaUUjrrJor^^^^^^^^SqUGkvv2Av^^^^YP|^r-                        -^|2i^^^^^^-chAa4A2wkU5ot^^-- |2C2A4kSy4UUZkhnyyeeaShk4ASwwwwG2-  ^tOM\n"+
+        "NNbJVe^^^^^- --- ^Xko5Syv^xyrr^^^^^^^up9qF^rr^^^^|aer^^r^-               ---^|IJrvUF^^^^^^^vS4AaawE999w1- ---^Joy2Ame-^r3m4kPjC3yea5Xkw4S5w5h9mv- ^tOM\n"+
+        "NNblv^^^^^--^--^^-^ekaS46Us4errr^^^^^^vkkkPCv||^^7hl^^^^^rr^----^^^r|v7xsCoaAIr^iGu^^^^^^^|aSAaPU9UEqe^      -C2e2U3- -L^jEmPzyeo2SwwUPSSw5w9S3kr ^tOM\n"+
+        "NNOJvr^^^-rr-^^^----r5GkAwkUm4vrrr^^^^^ryPPj^^^ -i4i^^^^^^^rtYJs2SA2eyy3jt1^^-^fEy^^^^^^^v4SAZPkUUmmL-       ^5o2Ua^   ^vtGqmwZSaSZ44ZXkPkkUO9rfa-^tOM\n"+
+        "NNbtv^^^ r1^^^rr^----^yEPU9UX4PV|rr^^^^^-|zAki- -vAJ^^^^rr|JfT7i^^r^^^^^^^^^^^jUJ^^^^^^^Jk5ZhPhP9qC^         TE4kv-rr-^vAauYffFsssssuAASk6UE99E6RAitOM\n"+
+        "NNOJv^^ -rr^^^^rCyvrr^^F6PXPkk2aAL|r^^^- ---rJv^-r2i^rrtIvr^^^rv^^^^^^^^^^^^rV2v^^^--^ckk5PkkPUmA^          ^EqP^  -fPsTxfFsszzzzjzjF7tlF2U6k6mqmbstOM\n"+
+        "NNbJLr^rv|rrr^^uPS52Yv^-1PaA2eo2yzF|^-       ----r2Trt7v^--- -^vv^^^^^^^^^|t2zr- --rCGU5kUUPkGPr-          -eU|- -|oCsJIYYFssjzzjzjjYtlye22eSdOdpDstOM\n"+
+        "NNbxi|vtu|rrrrv5hPkkytv|vV4e3eAejfeT-            ^3jT||ir^-----^ir^^^^^--^I23oyuy4GGw5kEkPP5jr-   ---    -rv^   ^waFAAy442VujnnjsjyeyJv||LlYT|iyUbztOM\n"+
+        "NQOJT|^|lrrrrrFEPUUolxteAya2sItloejv-            -LeTr1vr^^^^^^|t^-    -vSY^ -rLAUS4h542Ccr^--    ----   --    ^94CoZAye22AaAA2oAIrrriCFyXZkEAtJvr|tON\n"+
+        "QQ8YtJLtTr^rrreGkEkjJtJ4S3Aw3TLvilyc-             ^as|r|rrvrr1|Jl^    ^Ca|- -^Lyoe2ezcr^^^^-     ------       ^Hk2ya5Z5S4w4oeey2Sr^JezTAaw9hSc^vzl7l8N\n"+
+        "QQN08dZstL|rrr2Gk6kzt7sS2CA4Af|1||TL-             -Torrvietrvir^-   -vAIrrLzAaeCJ|r^^^^^^-       ------      -ADSAVA2y2k5eCCCzFze|^TCESvr1rvlTT3PH80MQ\n"+
+        "QQQQNNMRmVtTLvAmU6UjtcjAyuAAASu|rrv|-              ^2z^^^^^^--    -rAI^  ------^--^^^--          ------      rDaSSS4ZU5yCy3jFFzsyirTCoXPoT||t2OgMNNQQQ\n"+
+        "QQQQQQQNNRSxTvVdEGECJcsSojV4a5t^rrLr               -LUr         -r2u^- -----^^^^----          -  ------     -om4S5SU62yooenszssf3t^|oneuTi7lUgMNNQQQQQ\n"+
+        "QQQQQQQQNMgGlLT9qUG2x7JSS2zICSA^-Jx-                ^eo^      -^yer-    -------                - -- ----    ^Gk5kk9SFsnzuCCCe2225y^^jzPi^|J9gNNQQQQQQQ\n"+
+        "QQQQQQQQQQNWAJtxsIfxtttxlxJttJxLvttv|vvvvvvvvvvvvv||vtl7vvvvv|vJfT||vvv||||||||||vvvvvvvvvvvvv|vvvvvvvvvvvv|iltxfFYJttttJttttJJxIlTvTtTLvTAgNQQQQQQQQQ\n"+
+        "QQQQQQQQQQQNg888RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR8RRR8RRRRRRRRRRRRRR8RR8RRR8RgQQQQQQQQQQQ";
+    
         System.out.println("Hello from\n" + logo);
     }
 }
-
