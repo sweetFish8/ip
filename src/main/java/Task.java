f41bae3c9bddf361b0b55068aedd5c1b0348b510
Ayu <achanhaaaa@icloud.com>
2025-02-07 04:00:36 +0800

    Add markAsUndone method to Task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,17 +1,21 @@
 public class Task {
-    protected String description;
-    protected boolean isDone;
+  protected String description;
+  protected boolean isDone;
 
-    public Task(String description) {
-        this.description = description;
-        this.isDone = false;
-    }
+  public Task(String description) {
+    this.description = description;
+    this.isDone = false;
+  }
 
-    public String getStatusIcon() {
-        return (isDone ? "X" : " "); // mark done task with X
-    }
+  public String getStatusIcon() {
+    return (isDone ? "X" : " "); // mark done task with X
+  }
 
-    public void markAsDone() {
-        this.isDone = true;
-    }
+  public void markAsDone() {
+    this.isDone = true;
+  }
+
+  public void markAsUndone() {
+    this.isDone = false;
+  }
 }
